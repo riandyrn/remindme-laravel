b06e01942c6e9a0b5eee763abfdc80c59fc30be9
Aditya Ghaffar <adit.bisa@gmail.com>
2023-12-10 03:33:44 +0700

    feat: disable sanctum stateful auth

--- src/config/sanctum.php
+++ src/config/sanctum.php
@@ -1,6 +1,6 @@
 <?php
 
-use Laravel\Sanctum\Sanctum;
+// use Laravel\Sanctum\Sanctum;
 
 return [
 
@@ -15,11 +15,12 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
-    ))),
+    'stateful' => [],
+    // 'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    //     '%s%s',
+    //     'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+    //     Sanctum::currentApplicationUrlWithPort()
+    // ))),
 
     /*
     |--------------------------------------------------------------------------
@@ -33,7 +34,8 @@ return [
     |
     */
 
-    'guard' => ['web'],
+    'guard' => [],
+    // 'guard' => ['web'],
 
     /*
     |--------------------------------------------------------------------------
@@ -46,7 +48,7 @@ return [
     |
     */
 
-    'expiration' => null,
+    'expiration' => 1 / 60 * 20,
 
     /*
     |--------------------------------------------------------------------------
@@ -74,10 +76,11 @@ return [
     |
     */
 
-    'middleware' => [
-        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
-    ],
+    'middleware' => [],
+    // 'middleware' => [
+    //     'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
+    //     'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
+    //     'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+    // ],
 
 ];
