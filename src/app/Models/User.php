3870f07058b38f7a199c42ebd199333ed0a0cecd
Granite Bagas <granitebagas28@gmail.com>
2023-12-10 22:22:22 +0700

    Update model and migration

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -30,7 +30,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -39,7 +38,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 }
