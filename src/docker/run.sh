878aebb35ce0f22dcd5a9b99a1f164c83f1ccac5
Granite Bagas <granitebagas28@gmail.com>
2023-12-16 13:45:25 +0700

    Update docker and ci

--- src/docker/run.sh
+++ src/docker/run.sh
@@ -2,7 +2,7 @@
 
 cd /var/www
 
-composer install --optimize-autoloader --no-dev
+composer install -q --no-ansi --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
 
 cp .env.example .env
 php artisan key:generate
